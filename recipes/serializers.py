9cd7def88a3125ba034addd10e7502b1cd03100a
WandersoNoleto <onlywanderson@gmail.com>
2024-03-07 14:26:38 -0300

    (#001) add validadors to models: recipes

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,4 +1,7 @@
 from rest_framework import serializers
+from collections import defaultdict
+
+from attr import attr
 
 class RecipeSerializer(serializers.Serializer):
     id          = serializers.IntegerField()
@@ -12,5 +15,28 @@ class RecipeSerializer(serializers.Serializer):
     def get_preparation(self, recipe):
         return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
     
+    def validate(self, attrs):
+        super_validate = super().validate(attrs)
+
+        title = attrs.get('title')
+        description = attrs.get('description')
+
+        if title == description:
+            raise serializers.ValidationError(
+                {
+                    "title": ["Posso", "ter", "mais de um erro"],
+                    "description": ["Posso", "ter", "mais de um erro"],
+                }
+            )
+
+        return super_validate
+
+    def validate_title(self, value):
+        title = value
+
+        if len(title) < 5:
+            raise serializers.ValidationError('Must have at least 5 chars.')
+
+        return title
     def get_tags_name(self, recipe):
         return [tag.name for tag in recipe.tags.all()]
--- recipes/views.py
+++ recipes/views.py
@@ -15,6 +15,8 @@ def recipe_list(request):
     elif request.method == 'POST':
         serializer = RecipeSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        #serializer.save()
+        return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
 
 
 @api_view()

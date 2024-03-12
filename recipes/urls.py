7533a31db2d13ae3a92e190ba84dddb5312ce840
WandersoNoleto <onlywanderson@gmail.com>
2024-03-12 09:58:30 -0300

    add JWT token (Access&Validate)

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path, include
 from recipes import views
 from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 app_name = 'recipes'
 recipe_router = SimpleRouter()
@@ -11,5 +12,8 @@ recipe_router.register(
 )
 
 urlpatterns = [
-    path('', include(recipe_router.urls))
+    path('', include(recipe_router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -2,11 +2,30 @@ from recipes.models import Recipe
 from recipes.serializers import RecipeSerializer
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
-
+from recipes.permissions import IsOwner
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import status
+from rest_framework.response import Response
 
 class RecipeAPIViewSet(ModelViewSet):
     queryset         = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     filter_backends  = [DjangoFilterBackend]
     filterset_fields = ['category', 'tags']
+    permission_classes = [IsAuthenticatedOrReadOnly]
+        
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'ṔOST']:
+            return [IsOwner(),]
+        
+        return super().get_permissions()
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=request.user)
+        
+        headers = self.get_success_headers(serializer.data)
+        
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
     
